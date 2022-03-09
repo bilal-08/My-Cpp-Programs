bc8e32812d4ae5b66b0725d199f27c78dc01e3c0
RedBeeツ <77657831+bilal-08@users.noreply.github.com>
2022-03-09 21:01:06 +0530

    Create Practical_1.cpp

---
+++ Basic/Practical_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+string user, address;
+int roll_no;
+char division;
+
+user = "John doe ";
+address = "123 street California";
+roll_no = 16;
+division = 'A';
+
+cout << user << endl << address << endl << roll_no << endl << division;
+return 0;
+
+}
